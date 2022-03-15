b5a38655bf029347fc67e512620737ec5302a826
Mass23 <massimo.bourquin@gmail.com>
2022-03-15 10:57:15 +0100

    copied files

---
+++ scripts/1_1_feature_importance.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn import ensemble
+from sklearn import model_selection
+from sklearn import inspection
+
+def FeatureImportance(X, y):    
+    # Model    
+    rkf = model_selection.RepeatedKFold(n_splits=5, n_repeats=5)
+    reg = ensemble.HistGradientBoostingRegressor(loss='poisson',
+                                        max_bins=100)
+    cv_res = model_selection.cross_validate(reg, X, y, cv=rkf, 
+                                            scoring='r2',
+                                            return_estimator=True)
+    
+    # Importance of features
+    imp_df = np.empty((0,X.shape[1]))
+    for idx, estimator in enumerate(cv_res['estimator']):
+        imp_res = inspection.permutation_importance(estimator, X, y, n_repeats=5, random_state=0)
+        imp_df = np.vstack([imp_df, imp_res.importances_mean])
+
+    return([cv_res['test_score'].mean()] + np.mean(imp_df, axis=0).tolist())
+
+# Data loading
+metadata = pd.read_csv('Data/Metadata/102GL_meta_clim.csv')
+metadata = metadata[~np.isnan(metadata['sba [cells g-1]'])]
+
+data = pd.read_csv('data/16S/NOMIS_16S_table_0122_filtered.csv')
+tax_data = pd.read_csv('data/16S/NOMIS_16S_taxonomy_0122_filtered.csv')
+
+metadata = metadata[metadata.patch.isin(data.columns)]
+data.index = data.Feature_ID
+print(data.shape)
+
+# Data formatting
+features = ['water_temp [°C]', 'ph [pH]', 'do [mg l-1]',
+            'do_sat [saturation]', 'w_co2 [mATM]', 'conductivity [uS cm -1]',
+            'turb [NTU]', 'lat_sp [DD]', 'ele_sp [m]', 'gl_sa [km2]', 'gl_cov [%]', 
+            'n3_srp [ug l-1]', 'n4_nh4 [ug l-1]',
+            'n5_no3 [ug l-1]', 'n6_no2 [ug l-1]', 'bio10', 'bio11', 'bio12',
+            'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 'bio1',
+            'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'fcf',
+            'fgd', 'scd', 'swe', 'pr', 'tas', 'tasmin', 'tasmax']
+
+X = metadata[features]
+exps = pd.get_dummies(metadata.Expedition, prefix='exp')
+features.extend(exps.columns.tolist())
+print(exps.columns.tolist())
+X = np.hstack((X, exps))
+print(X.shape)
+print(len(features))
+
+imp_df = pd.DataFrame(columns = ['Species', 'R2'] + features)
+
+count=0
+for ASV in data.index:
+    count +=1
+    print(count, end='\r')
+    y = metadata.patch.map(lambda x: data.loc[ASV, data.columns == x].mean(axis=0))
+    
+    imp = FeatureImportance(X, y)
+    imp_df.loc[imp_df.shape[0]] = [ASV] + imp
+
+imp_df.to_csv('Data/importance_features.csv',index=False)
