bcdee1c89004c7554c85ab84508123e5f9bbadec
Mass23 <massimo.bourquin@lcsb-cdc-litcrit.uni.lux>
2022-03-15 11:08:11 +0100

    first try

--- scripts/1_1_feature_importance.py
+++ scripts/1_1_feature_importance.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn import ensemble
 from sklearn import model_selection
@@ -14,36 +12,37 @@ def FeatureImportance(X, y):
                                         max_bins=100)
     cv_res = model_selection.cross_validate(reg, X, y, cv=rkf, 
                                             scoring='r2',
+                                            n_jobs=32,
                                             return_estimator=True)
     
     # Importance of features
     imp_df = np.empty((0,X.shape[1]))
     for idx, estimator in enumerate(cv_res['estimator']):
-        imp_res = inspection.permutation_importance(estimator, X, y, n_repeats=5, random_state=0)
+        imp_res = inspection.permutation_importance(estimator, X, y, n_repeats=5, random_state=0, n_jobs=32)
         imp_df = np.vstack([imp_df, imp_res.importances_mean])
 
     return([cv_res['test_score'].mean()] + np.mean(imp_df, axis=0).tolist())
 
 # Data loading
-metadata = pd.read_csv('Data/Metadata/102GL_meta_clim.csv')
+metadata = pd.read_csv('../data/102GL_meta_clim.csv')
 metadata = metadata[~np.isnan(metadata['sba [cells g-1]'])]
 
-data = pd.read_csv('data/16S/NOMIS_16S_table_0122_filtered.csv')
-tax_data = pd.read_csv('data/16S/NOMIS_16S_taxonomy_0122_filtered.csv')
+data = pd.read_csv('../data/NOMIS_16S_table_0122_filtered.csv')
+tax_data = pd.read_csv('../data/NOMIS_16S_taxonomy_0122_filtered.csv')
 
 metadata = metadata[metadata.patch.isin(data.columns)]
 data.index = data.Feature_ID
 print(data.shape)
 
 # Data formatting
-features = ['water_temp [°C]', 'ph [pH]', 'do [mg l-1]',
-            'do_sat [saturation]', 'w_co2 [mATM]', 'conductivity [uS cm -1]',
-            'turb [NTU]', 'lat_sp [DD]', 'ele_sp [m]', 'gl_sa [km2]', 'gl_cov [%]', 
-            'n3_srp [ug l-1]', 'n4_nh4 [ug l-1]',
-            'n5_no3 [ug l-1]', 'n6_no2 [ug l-1]', 'bio10', 'bio11', 'bio12',
-            'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 'bio1',
-            'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'fcf',
-            'fgd', 'scd', 'swe', 'pr', 'tas', 'tasmin', 'tasmax']
+features = ['water_temp [°C]', 'ph [pH]', 'do_sat [saturation]', 'w_co2 [mATM]', 'conductivity [uS cm -1]',
+            'turb [NTU]', 'lat_sp [DD]', 'ele_sp [m]', 'gl_sa [km2]', 'gl_cov [%]', 'chla [ug g-1]', 
+            'eps [ugC g-1]', 'ag [nmol g-1 h-1]', 'ap [nmol g-1 h-1]', 'bg [nmol g-1 h-1]', 'lap [nmol g-1 h-1]',
+            'nag [nmol g-1 h-1]', 'bp [ngC g-1 h-1]', 'respiration [mg 02 g-1 h-1]', 
+            'n3_srp [ug l-1]', 'n4_nh4 [ug l-1]', 'n5_no3 [ug l-1]', 'n6_no2 [ug l-1]', 
+            'bio10', 'bio11', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19', 'bio1',
+            'bio2', 'bio3', 'bio4', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'fcf', 'fgd', 'scd', 'swe', 'pr', 
+            'tas', 'tasmin', 'tasmax']
 
 X = metadata[features]
 exps = pd.get_dummies(metadata.Expedition, prefix='exp')
